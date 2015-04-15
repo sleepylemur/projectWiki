a7f00d6ed13669a483468f038d86376c1602c464
Evan Griffiths <griffithse@gmail.com>
2015-04-15 13:03:32 -0400

    ajax title verification coded

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var marked = require('marked');
 var sqlite3 = require('sqlite3');
 var express = require('express');
 var session = require('express-session');
@@ -47,14 +48,23 @@ app.use(bodyParser.urlencoded({
 
 // retrieve index of docs
 app.get("/", function (req,res) {
+  res.redirect("/docs");
+});
+
+app.get("/docs", function (req,res) {
   db.all("SELECT title,docs.docid,docs.version FROM docs JOIN versionedDocs ON docs.docid = versionedDocs.docid AND docs.version = versionedDocs.version", function(err,data) {
     if (err) throw(err);
     res.render("index.ejs",{docs: data});
   });
 });
 
+// retrieve new doc form
+app.get("/docs/new", function (req,res) {
+  res.render("new.ejs");
+});
+
 // retrieve doc by title
-app.get("/:title", function (req,res) {
+app.get("/doc/:title", function (req,res) {
   db.get("SELECT title,body FROM versionedDocs JOIN docs ON docs.docid = versionedDocs.docid AND docs.version = versionedDocs.version WHERE title = ?",
     req.params.title,
     function(err,data) {
@@ -62,13 +72,13 @@ app.get("/:title", function (req,res) {
       if (typeof data === 'undefined') {
         res.send(req.params.title + " not found.");
       } else {
-        res.render("doc.ejs",{title: data.title, body: data.body});
+        res.render("doc.ejs",{title: data.title, body: marked(data.body)});
       }
     });
 });
 
 // retrieve edit page for doc
-app.get("/:title/edit", function (req,res) {
+app.get("/doc/:title/edit", function (req,res) {
   db.get("SELECT title,body,docs.docid FROM versionedDocs JOIN docs ON docs.docid = versionedDocs.docid AND docs.version = versionedDocs.version WHERE title = ?",
     req.params.title,
     function(err,data) {
@@ -81,9 +91,21 @@ app.get("/:title/edit", function (req,res) {
     });
 });
 
+// ajax call to ensure titles are unique before submitting new and edit doc forms
+app.post("/validateTitle", function (req,res) {
+  // console.log(req.body.docid);
+  db.get("SELECT docid FROM versionedDocs WHERE title = ? AND docid != ?", req.body.title, req.body.docid, function (err,data) {
+    if (typeof data === "undefined") {
+      res.send(true);
+    } else {
+      res.send(false);
+    }
+  });
+});
+
 // process edit for doc ----- NEED to verify that title is unique BEFORE this
 // TODO lock database while updating. Include userid
-app.post("/:docid", function (req,res) {
+app.post("/doc/:docid", function (req,res) {
   db.get("SELECT max(version) FROM versionedDocs WHERE docid = ?", req.params.docid, function (err,data) {
     if (err) throw(err);
     if (typeof data === "undefined") { // maybe create a new entry here.
@@ -96,10 +118,23 @@ app.post("/:docid", function (req,res) {
           if (err) throw(err);
           db.run("UPDATE docs SET version = ? WHERE docid = ?", version, req.params.docid, function (err) {
             if (err) throw(err);
-            res.redirect("/"+req.body.title);
+            res.redirect("/doc/"+req.body.title);
           });
         });
     }
+  });
+});
+
+// post new doc ----- NEED to verify that title is unique BEFORE this
+app.post("/docs", function (req,res) {
+  db.run("INSERT INTO docs (version) VALUES (1)", function (err) {
+    if (err) throw(err);
+    db.run("INSERT INTO versionedDocs (docid, title, body, version, userid, changed) VALUES (?,?,?,?,?,strftime('%s','now'))",
+      this.lastID, req.body.title, req.body.body, 1, "guest",
+      function (err) {
+        if (err) throw(err);
+        res.redirect("/doc/"+req.body.title);
+      });
   });
 });
 
